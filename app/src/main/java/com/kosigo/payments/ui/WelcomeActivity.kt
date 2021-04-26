9f53277e93e0105f7f04ef6e0441dba6831771da
Kosigo <Djan.org@gmail.com>
2021-04-26 21:29:29 +0300

    welcome fragment

--- app/src/main/java/com/kosigo/payments/ui/WelcomeActivity.kt
+++ app/src/main/java/com/kosigo/payments/ui/WelcomeActivity.kt
@@ -4,29 +4,21 @@ import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.kosigo.payments.PaymentApplication
-import com.kosigo.payments.PaymentApplication.Companion.TOKEN
+import androidx.lifecycle.lifecycleScope
 import com.kosigo.payments.R
-import com.kosigo.payments.ui.auth.AuthFragment
+import kotlinx.coroutines.delay
 
 
 class WelcomeActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val token = PaymentApplication.instance.sharedPreferences.getString(TOKEN,"") ?:""
-        if(token.isNotEmpty()){
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-            return
-        }
-
-        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         setContentView(R.layout.welcome_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, AuthFragment.newInstance())
-                .commitNow()
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        lifecycleScope.launchWhenCreated {
+            delay(20000)
+            val intent = Intent(this@WelcomeActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
